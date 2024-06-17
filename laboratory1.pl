626f5abad1cd226df759680393f15c41642d7521
BiB-Si <116101491+BiB-Si@users.noreply.github.com>
2024-06-17 23:35:28 +0800

    Update laboratory1.pl

--- laboratory1.pl
+++ laboratory1.pl
@@ -3,15 +3,22 @@ colour(peach, yellow).
 colour(apple, red).
 colour(apple, yellow).
 
-
 fruits(apple).
 fruits(peach).
-
 
 likes(mary, peach).
 likes(mary, kukuruza).
 likes(mary, apple).
 
-
+%что любит Beth ?- likes(beth, X). X = apple X = kukuruza
 likes(beth, B) :- likes(mary, B), fruits(B), colour(B, red).
 likes(beth, B) :- likes(mary, B), B = kukuruza.
+% ?-knownfruits(F). F = apple F = peach
+knownfruits(F) :- fruits(F).
+
+% Цвета фруктов, которые любят Beth и Mary ?- favoritefruits(Color). Color = red Color = yellow
+favoritefruits(Color) :-
+    likes(beth, Fruit),
+    likes(mary, Fruit),
+    fruits(Fruit),
+    colour(Fruit, Color).
